5080e18ad01e1b212897bf1353e88205b7af7f9d
madhupriya20049 <madhusashi2004@gmail.com>
2025-01-31 20:54:19 +0530

    training

---
+++ React/app/src/component/Cbc.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+class Cbc extends React.Component{
+    render(){
+        return <h1>I am class based component</h1>
+    }
+}
+export default Cbc;
---
+++ React/app/src/component/Fbc.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Fbc = ()=>{
+    return <h1>I am function based component</h1>
+}
+export default Fbc;
