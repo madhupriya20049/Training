406371c33b84271f40a80a310b454c9992facbec
madhupriya20049 <madhusashi2004@gmail.com>
2025-02-12 12:54:49 +0530

    fetch-useEffect

--- React/app/src/useEffect/UseEffect.jsx
+++ React/app/src/useEffect/UseEffect.jsx
@@ -40,22 +40,67 @@
 
 //dependency with value
 
+// import React, { useEffect, useState } from 'react'
+
+// const UseEffect = () => {
+//     let [state,setState]=useState(0)
+//     let [increment,setIncrement]=useState(0)
+//     useEffect(()=>{
+//         setIncrement(()=>state*2)
+//     },[state])
+//   return (
+//     <div>
+//       {state} <br/>
+//       <button onClick={()=> setState((e)=> e+1)}>click</button>
+//       <br/>
+//       {increment}
+//     </div>
+//   )
+// }
+
+// export default UseEffect
+
+//fetch api using useEffect hook
 import React, { useEffect, useState } from 'react'
+import './UseEffect.css'
 
 const UseEffect = () => {
-    let [state,setState]=useState(0)
-    let [increment,setIncrement]=useState(0)
-    useEffect(()=>{
-        setIncrement(()=>state*2)
-    },[state])
+  let [state,setState]=useState([])
+  console.log(state);
+  useEffect(()=>{
+     window.fetch('https://fakestoreapi.com/products')
+     .then((data)=>data.json())
+     .then((res)=>setState(res))
+  },[])
   return (
     <div>
-      {state} <br/>
-      <button onClick={()=> setState((e)=> e+1)}>click</button>
-      <br/>
-      {increment}
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>price</th>
+            <th>image</th>
+          </tr>
+        </thead>
+        {
+          state.map((res)=>{
+            console.log(res);
+            return(
+              <>
+              <tr>
+                <td>{res.id}</td>
+                <td>{res.price}</td>
+                <td><img src={res.image} alt=""/></td>
+                </tr>
+              </>
+            )
+          })
+        }
+      </table>
+      
     </div>
   )
 }
 
 export default UseEffect
+
